2cc613507441c12253294bb4f207dab03d7874c2
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-31 15:47:11 +0530

    home page done'

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  );
+};
+
+export default AuthLayout;
